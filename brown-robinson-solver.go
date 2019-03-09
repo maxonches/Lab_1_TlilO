d7cf13f88e0b836c18707df03837e6d7a4f5a15d
maxonches <maxonches@mail.ru>
2019-03-10 01:32:16 +0300

    Add files via upload

---
+++ brown-robinson-solver.go
@@ -0,0 +1,49 @@
+package brown_robinson
+
+import "gonum.org/v1/gonum/mat"
+
+type BrownRobinsonSolver struct {
+	payoffMatrix		*mat.Dense
+	brownRobinsonTable	*BrownRobinsonTable
+}
+
+func NewBrownRobinsonSolver(inputMatrix *mat.Dense) *BrownRobinsonSolver {
+	return &BrownRobinsonSolver{
+		inputMatrix,
+		nil,
+	}
+}
+
+func (brs *BrownRobinsonSolver) Solve(threshold float64) {
+	r, c := brs.payoffMatrix.Dims()
+	brt := NewBrownRobinsonTable(
+		brs.payoffMatrix,
+		RandFromRange(0, r),
+		RandFromRange(0, c),
+	)
+	brs.brownRobinsonTable = brt
+	brt.Solve(threshold, 1000)
+}
+
+func (brs *BrownRobinsonSolver) CalculateStrategyA() *mat.VecDense {
+	return brs.brownRobinsonTable.getAMixedStrategy()
+}
+
+func (brs *BrownRobinsonSolver) CalculateStrategyB() *mat.VecDense {
+	return brs.brownRobinsonTable.getBMixedStrategy()
+}
+
+func (brs *BrownRobinsonSolver) CalculateGameCost() float64 {
+	aMixedStrategy := brs.CalculateStrategyA()
+	bMixedStrategy := brs.CalculateStrategyB()
+
+	gameCost := 0.0
+	for j := 0; j < bMixedStrategy.Len(); j++ {
+		for i := 0; i < aMixedStrategy.Len(); i++ {
+			gameCost += brs.payoffMatrix.At(i, j) * aMixedStrategy.AtVec(i) * bMixedStrategy.AtVec(j)
+		}
+	}
+
+	return gameCost
+}
+
