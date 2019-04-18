b2abc39f9418656c3a84ed06198e248d932b4d3f
maxonches <maxonches@mail.ru>
2019-04-18 23:45:33 +0300

    some fixes/ it is done

--- lab4/main.go
+++ lab4/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/dglo/java2go/parser"
-	"go/format"
 	"gonum.org/v1/gonum/mat"
-	"math"
 	"math/rand"
 	"time"
 )
 
-const bOUNDARIES = 50
-const mATRIX_SIZE = 10
-const aNSI_RESET = "\u001B[0m"
-const aNSI_RED = "\u001B[31m"
-const aNSI_GREEN = "\u001B[32m"
-const aNSI_BLUE = "\u001B[34m"
-
-type LR9Main struct {
-
-}
+const BOUNDARIES = 50
+const MATRIX_SIZE = 10
+const SET = "\u001B[0m"
+const COLOR_RED = "\u001B[31m"
+const COLOR_GREEN = "\u001B[32m"
+const COLOR_BLUE = "\u001B[34m"
 
 func main() {
 	inputBiMatrix := [][][]float64{
@@ -38,7 +31,7 @@ func main() {
 			{2, 0.5}, {0, 0},
 		},
 	}
-	battleOFMalesBiMatrix := [][][]float64{
+	battleOfMalesBiMatrix := [][][]float64{
 		{
 			{4, 1}, {0, 0},
 		},
@@ -46,7 +39,7 @@ func main() {
 			{0, 0}, {1, 4},
 		},
 	}
-	pRISONER_DILEMMA_BI_MATRIX := [][][]float64{
+	prisonerDilemmaBiMatrix := [][][]float64{
 		{
 			{-5, -5}, {0, -10},
 		},
@@ -55,29 +48,25 @@ func main() {
 		},
 	}
 
-	fmt.Println(fmt.Sprintf("%v%v%v", aNSI_RED, "Равновесие по Нэшу", aNSI_RESET))
-	fmt.Println(fmt.Sprintf("%v%v%v", aNSI_BLUE, "Парето эффективность", aNSI_RESET))
-	fmt.Println(fmt.Sprintf("%v%v%v", aNSI_GREEN, "Все вместе", aNSI_RESET))
+	fmt.Println(fmt.Sprintf("%v%v%v", COLOR_RED, "Равновесие по Нэшу", SET))
+	fmt.Println(fmt.Sprintf("%v%v%v", COLOR_BLUE, "Парето эффективность", SET))
+	fmt.Println(fmt.Sprintf("%v%v%v", COLOR_GREEN, "Все вместе", SET))
 	fmt.Println("Перекресток:")
-	findAllOptimalSituations(crossroadBiMatrix)
+	SearchAllOptimalSituations(crossroadBiMatrix)
 	fmt.Println("Семейный спор:")
-	findAllOptimalSituations(battleOFMalesBiMatrix)
+	SearchAllOptimalSituations(battleOfMalesBiMatrix)
 	fmt.Println("Дилемма заключенного:")
-	findAllOptimalSituations(pRISONER_DILEMMA_BI_MATRIX)
-	inputBiMatrix := generateBiMatrix(mATRIX_SIZE, bOUNDARIES)
+	SearchAllOptimalSituations(prisonerDilemmaBiMatrix)
+	inputMatrix := GenerateBiMatrix(MATRIX_SIZE, BOUNDARIES)
 	fmt.Println()
 	fmt.Println("Случайно сгенерированная матрица 10х10:")
-	findAllOptimalSituations(inputBiMatrix)
-	fmt.Println("Матрица по варианту")
-	findAllOptimalSituations(inputBiMatrix)
-	solveAnalitycally(inputBiMatrix)
+	SearchAllOptimalSituations(inputMatrix)
+	fmt.Println("Матрица по варианту:")
+	SearchAllOptimalSituations(inputBiMatrix)
+	SolveAnalitycal(MATRIX_SIZE, inputBiMatrix)
 }
 
-func NewLR9Main() (rcvr *LR9Main) {
-	rcvr = &LR9Main{}
-	return
-}
-func checkForNashOptimality(matrix [][][]float64, i int, j int) bool {
+func CheckEquilibriumByNash(matrix [][][]float64, i int, j int) bool {
 	isThereBetterStrategyForA := false
 	isThereBetterStrategyForB := false
 	for jIter := 0; jIter < len(matrix); jIter++ {
@@ -92,7 +81,8 @@ func checkForNashOptimality(matrix [][][]float64, i int, j int) bool {
 	}
 	return !(isThereBetterStrategyForA || isThereBetterStrategyForB)
 }
-func checkForParretoEfficiency(matrix [][][]float64, i int, j int) (bool) {
+
+func СheckParetoOptimality(matrix [][][]float64, i int, j int) (bool) {
 	isThereBetterStrategy := false
 	for iter := 0; iter < len(matrix); iter++ {
 		for jIter := 0; jIter < len(matrix); jIter++ {
@@ -105,20 +95,21 @@ func checkForParretoEfficiency(matrix [][][]float64, i int, j int) (bool) {
 
 	return !isThereBetterStrategy
 }
-func findAllOptimalSituations(inputMatrix [][][]float64) {
+
+func SearchAllOptimalSituations(inputMatrix [][][]float64) {
 	for i := 0; i < len(inputMatrix); i++ {
 		for j := 0; j < len(inputMatrix[0]); j++ {
-			isNashEfficient := checkForNashOptimality(inputMatrix, i, j)
-			isParetoOptimal := checkForParretoEfficiency(inputMatrix, i, j)
+			isNashEfficient := CheckEquilibriumByNash(inputMatrix, i, j)
+			isParetoOptimal := СheckParetoOptimality(inputMatrix, i, j)
 			result := fmt.Sprintf("%v%v%v%v%v", "(", inputMatrix[i][j][0], ";", inputMatrix[i][j][1], ")")
 			if isNashEfficient && isParetoOptimal {
-				result = fmt.Sprintf("%v%v%v", aNSI_GREEN, result, aNSI_RESET)
+				result = fmt.Sprintf("%v%v%v", COLOR_GREEN, result, SET)
 			}
 			if isNashEfficient && !isParetoOptimal {
-				result = fmt.Sprintf("%v%v%v", aNSI_RED, result, aNSI_RESET)
+				result = fmt.Sprintf("%v%v%v", COLOR_RED, result, SET)
 			}
 			if !isNashEfficient && isParetoOptimal {
-				result = fmt.Sprintf("%v%v%v", aNSI_BLUE, result, aNSI_RESET)
+				result = fmt.Sprintf("%v%v%v", COLOR_BLUE, result, SET)
 			}
 			fmt.Print(fmt.Sprintf("%v%v", result, " "))
 		}
@@ -126,88 +117,80 @@ func findAllOptimalSituations(inputMatrix [][][]float64) {
 	}
 }
 
-func generateBiMatrix(size int, boundaries float64) ([][][]float64) {
+func GenerateBiMatrix(size int, boundaries float64) ([][][]float64) {
 	rand.Seed(time.Now().Unix())
+	size = 10
+	boundaries = 50.0
 	min := -1 * boundaries
-	max := boundaries
-	matrix := make([][][]float64, size)
-	for i := 0; i < len(matrix); i++ {
-		matrixOneDimensional := make([][]float64, size)
-		for j := 0; j < len(matrix); j++ {
-			matrixTwoDimensional := make([]float64, 2)
-			matrix[i][j][0] = rand.Float64() * (max - min) + min
-			matrix[i][j][1] = rand.Float64() * (max - min) + min
-		}
-		matrix = append(matrix, matrixOneDimensional)
-	}
-
-	return matrix
-}
-
-func generateBiMatrix(size int, boundaries float64) ([][][]float64) {
-	rand.Seed(time.Now().Unix())
-	min := -1 * boundaries
-	max := boundaries
-	matrix := make([][][]float64, size)
-	for i := 0; i < len(matrix); i++ {
-		matrixOneDimensional := make([][]float64, size)
-		for j := 0; j < len(matrix); j++ {
-			matrixTwoDimensional := make([]float64, 2)
-			matrix[i][j][0] = rand.Float64() * (max - min) + min
-			matrix[i][j][1] = rand.Float64() * (max - min) + min
-		}
-		matrix = append(matrix, matrixOneDimensional)
-	}
-
-	return matrix
-}
-
-func GENERATE() {
-	size := 10
-	boundaries := 50
-	rand.Seed(time.Now().Unix())
-	matrix := make([][][]float64, size)
+	max := 50.0
+	matrix := make([][][]float64, 0, size)
 	for i := 0; i < size; i++ {
-		matrix[i] = make([][]float64, size + 1)
-		for j := 0; j < size + 1; j++ {
-			matrix[i][j] = make([]float64, boundaries)
-			for k := 0; k < int(boundaries); k++ {
-				matrix[i][j][k] = 1
+		matrixOneDimensional := make([][]float64, 0, size)
+		for j := 0; j < size; j++ {
+			matrixTwoDimensional := make([]float64, 0, 2)
+			for k := 0; k < 2; k++ {
+				matrixTwoDimensional = append(matrixTwoDimensional, float64(rand.Intn(int(max)-int(min))+int(min)))
 			}
+			matrixOneDimensional = append(matrixOneDimensional, matrixTwoDimensional)
 		}
+		matrix = append(matrix, matrixOneDimensional)
 	}
-	fmt.Println(matrix)
+
+	return matrix
 }
 
-func solveAnalitycally(matrix [][][]float64) {
-	matrixA := make([]float64, len(matrix), len(matrix))
-	matrixB := make([]float64, len(matrix), len(matrix))
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix); j++ {
-			matrixA[i][j] = matrix[i][j][0]
-			matrixB[i][j] = matrix[i][j][1]
+func SolveAnalitycal(size int, inputBiMatrix [][][]float64) {
+	matrixAOneDim := make([]float64, 0, size*size)
+	matrixBOneDim := make([]float64, 0, size*size)
+	for i := 0; i < len(inputBiMatrix); i++ {
+		for j := 0; j < len(inputBiMatrix); j++ {
+			matrixAOneDim = append(matrixAOneDim, inputBiMatrix[i][j][0])
+			matrixBOneDim = append(matrixBOneDim, inputBiMatrix[i][j][1])
 		}
 	}
-	uRow := make([]float64, len(matrix))
-	for i := 0; i < len(matrix); i++ {
+	matrixA := mat.NewDense(len(inputBiMatrix), len(inputBiMatrix), matrixAOneDim)
+	matrixB := mat.NewDense(len(inputBiMatrix), len(inputBiMatrix), matrixBOneDim)
+
+	uRow := make([]float64, len(inputBiMatrix))
+	for i := 0; i < len(inputBiMatrix); i++ {
 		uRow[i] = 1
 	}
-	u := []float64{uRow}
-	u_t := make([]float64, len(matrix), 1)
-	for i := 0; i < len(matrix); i++ {
-		u_t[i][0] = 1
-	}
-	aReverse := mat.calculateReverseMatrix(matrixA)
-	bReverse := mat.calculateReverseMatrix(matrixB)
-	aReverseUT := mat.multiplicate(aReverse, u_t)
-	bReverseUT := mat.multiplicate(bReverse, u_t)
-	vX := 1 / mat.multiplicate(u, aReverseUT)[0][0]
-	vY := 1 / mat.multiplicate(u, bReverseUT)[0][0]
-	format := NewDecimalFormat("#0.000")
-	fmt.Println(fmt.Sprintf("%v%v", "VX:", format.format(vX)))
-	fmt.Println(fmt.Sprintf("%v%v", "VY:", format.format(vY)))
-	x := []float64{MatrixTools.multiplicate(aReverse, u_t)[0][0] * vX, MatrixTools.multiplicate(aReverse, u_t)[1][0] * vX}
-	y := []float64{MatrixTools.multiplicate(u, bReverse)[0][0] * vY, MatrixTools.multiplicate(u, bReverse)[0][1] * vY}
-	fmt.Println(fmt.Sprintf("%v%v", "X:", fmt.Sprintf("%v", x)))
-	fmt.Println(fmt.Sprintf("%v%v", "Y:", fmt.Sprintf("%v", y)))
+	u := mat.NewVecDense(len(inputBiMatrix), uRow)
+
+	var inverseMatrixA, inverseMatrixB mat.Dense
+	inverseMatrixA.Inverse(matrixA)
+	inverseMatrixB.Inverse(matrixB)
+
+	uAinv := mat.Dense{}
+	uAinv.Product(u.T(), &inverseMatrixA)
+	uAinvUt := mat.Dense{}
+	uAinvUt.Product(&uAinv, u)
+	v1 := 1 / uAinvUt.At(0,0)
+
+	uBinv := mat.Dense{}
+	uBinv.Product(u.T(), &inverseMatrixB)
+	uBinvUt := mat.Dense{}
+	uBinvUt.Product(&uBinv, u)
+	v2 := 1 / uBinvUt.At(0,0)
+
+	fmt.Println(fmt.Sprintf("%v%.3f", "v1 = ", v1))
+	fmt.Println(fmt.Sprintf("%v%.3f", "v2 = ", v2))
+
+	x := mat.Dense{}
+	x.Product(u.T(), &inverseMatrixB)
+	x.Scale(v2, &x)
+
+	y := mat.Dense{}
+	y.Product(&inverseMatrixA, u)
+	y.Scale(v1, &y)
+
+	fmt.Println("x = ")
+	MatPrint(&x)
+	fmt.Println("y = ")
+	MatPrint(y.T())
+}
+
+func MatPrint(x mat.Matrix) {
+	fa := mat.Formatted(x, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%.3f\n", fa)
 }
